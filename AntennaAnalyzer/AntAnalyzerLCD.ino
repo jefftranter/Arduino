340bb3f37cc2f5e28a53f89757807536f664564b
Jeff Tranter <tranter@pobox.com>
2016-06-08 21:42:07 -0400

    Clean up and simplify code.

--- AntennaAnalyzer/AntAnalyzerLCD.ino
+++ AntennaAnalyzer/AntAnalyzerLCD.ino
@@ -1,4 +1,10 @@
-#include <SoftwareSerial.h>
+/*
+ * Arduino and AD8950-based Antenna Analyzer.
+ *
+ * Original by J M Harvey. See https://github.com/jmharvey1/DDS_AD9850_AntennaAnalyzer
+ * Modified by Jeff Tranter <tranter@pobox.com>
+ *
+ */
 
 /*
 
@@ -14,12 +20,7 @@ serial.write(254); serial.write(145); serial.write(128);
 
 */
 
-/* 
- * Arduino and AD8950-based Antenna Analyzer.
- *
- * Original by  J M Harvey. See https://github.com/jmharvey1/DDS_AD9850_AntennaAnalyzer
- * Modified by Jeff Tranter <tranter@pobox.com>
- */
+#include <SoftwareSerial.h>
  
 #define W_CLK 8       // Pin 8  - connect to AD9850 module word load clock pin (CLK)
 #define FQ_UD 9       // Pin 9  - connect to freq update pin (FQ)
@@ -28,20 +29,15 @@ serial.write(254); serial.write(145); serial.write(128);
  
 #define pulseHigh(pin) { digitalWrite(pin, HIGH); digitalWrite(pin, LOW); }
 
-double Start_Freq =  2.0e6;  // Start frequency in Hz
-double End_Freq   =  23.0e6; // End frequency in Hz
-double Step_Freq  =  25000;  // Step frequency in Hz
-double current_freq;
-int FwdOffSet;
-int RevOffSet;
-int FwdSCVal = 30;     // Set to forward reading found when antenna leg of bridge is shorted; Diode sensitivity compensation.
-int RevSCVal = 40;     // Set to reverse reading found when antenna leg of bridge is shorted; Diode sensitivity compensation.
-int FwdOpAmpGain = 92; // Set to forward reading found when cathodes of D1 and D2 are shorted together; (Op Amp Gain loop compensation).
-int RevOpAmpGain = 88; // Set to reverse reading found when cathodes of D1 and D2 are shorted together; (Op Amp Gain loop compensation).
+double Start_Freq =  7000000; // Start frequency in Hz
+double End_Freq   =  7300000; // End frequency in Hz
+double Step_Freq  =    10000; // Step frequency in Hz
+double current_freq;          // Current frequency
+int Readings = 50;            // Number of readings to average over.
 
 SoftwareSerial mySerial(2, 3); // RX, TX
-    
-// Transfers a byte, a bit at a time, LSB first to the 9850 via serial DATA line
+
+// Transfers a byte, a bit at a time, LSB first to the 9850 via serial DATA line.
 void tfr_byte(byte data)
 {
     for (int i = 0; i < 8; i++, data >>= 1) {
@@ -58,7 +54,7 @@ void sendFrequency(double frequency)
         tfr_byte(freq & 0xFF);
     }
     tfr_byte(0x000);   // Final control byte, all 0 for 9850 chip
-    pulseHigh(FQ_UD);  // Done!  Should see output
+    pulseHigh(FQ_UD);  // Done! Should see output
 }
 
 void setup()
@@ -71,37 +67,24 @@ void setup()
    
     pulseHigh(RESET);
     pulseHigh(W_CLK);
-    pulseHigh(FQ_UD);  // This pulse enables serial mode - Datasheet page 12 figure 10
+    pulseHigh(FQ_UD);  // This pulse enables serial mode - datasheet page 12 figure 10
     current_freq = Start_Freq;
+
     mySerial.begin(19200);
+    Serial.begin(9600);
  }
 
 void loop()
 {
-    int incomingByte = 0;   // For incoming serial data
-    bool RunCurve = true;
-  
-    if (mySerial.available() > 0) {
-        // Read the incoming byte:
-        while (mySerial.available()) {
-            incomingByte = mySerial.read();
-            //mySerial.println(incomingByte);
-        }
-        RunCurve = !RunCurve;
-    } else {
-        sendFrequency(1.0);  // Set AD9850 output to 1 Hz
-        delay(200);
-        RevOffSet = analogRead(A0);
-        FwdOffSet = analogRead(A1);
-        //delay(2000);
-    }
-
-    while (RunCurve) {
-         RunCurve = PrintNextPoint(RunCurve);
+    bool done = false;
+    Serial.println("Start Scan");
+    delay(1000);
+    while (!done) {
+         done = PrintNextPoint();
     }
 }
 
-bool PrintNextPoint(bool RunCurve)
+bool PrintNextPoint()
 {
     double FWD = 0.0;
     double REV = 0.0;
@@ -109,62 +92,74 @@ bool PrintNextPoint(bool RunCurve)
     double EffOhms;
     if (current_freq > End_Freq) {
         current_freq = Start_Freq;
-        RunCurve = !RunCurve;
-        mySerial.println("Scan Complete");
-        return RunCurve; 
+        return true;
     }
     sendFrequency(current_freq);  // Frequency
     delay(100);
-    // Read the forawrd and reverse voltages
-    for (int i = 0; i < 70; i++) {
-         REV += (analogRead(A0) - RevOffSet);
-         FWD += (analogRead(A1) - FwdOffSet);
+    // Read the forward and reverse voltages. Average over Readings readings.
+    for (int i = 0; i < Readings; i++) {
+         REV += analogRead(A0);
+         FWD += analogRead(A1);
     }
-    REV = REV / 70.0;
-    FWD = FWD / 70.0;    
-    REV = (FwdOpAmpGain * REV) / RevOpAmpGain; // Apply Op Amp Gain loop compensation
-    REV = CorrectReading(REV); // Now using table apply small signal correction value
-    
-    FWD = (RevSCVal * FWD) / FwdSCVal; // Apply "short circuit" offset
-    FWD = CorrectReading(FWD); // Now using table apply small signal correction value
+
+    REV = REV / (double)Readings; // Compensate for reading multiple times.
+    FWD = FWD / (double)Readings;
+    //Serial.print("FWD="); Serial.print(FWD);
+    //Serial.print(" REV="); Serial.println(REV);
+    REV = sqrt(REV);
+    FWD = sqrt(FWD);
+
     if (REV >= FWD) {
         // To avoid a divide by zero or negative VSWR then set to max 999
         VSWR = 999;
     } else {
         // Calculate VSWR
-        VSWR = ((FWD + REV) / (FWD - REV));
+        VSWR = (FWD + REV) / (FWD - REV);
     }
-    if (FWD >= 115)
-        EffOhms = VSWR * 50.0; // FWD >= 94
+    if (FWD > 24) // Experimentally found this was point of lowest/matched SWR
+        EffOhms = VSWR * 50.0;
     else
         EffOhms = 50.0 / VSWR;
 
+    // Send current line back to PC over serial bus.
+    // e.g.
+    // Freq: 7160 kHz Fwd: 24.21 Rev: 5.92 SWR: 1.65 Ohms: 82.39
+    Serial.print("Freq: ");
+    Serial.print(int(current_freq / 1000));
+    Serial.print(" kHz Fwd: ");
+    Serial.print(FWD);
+    Serial.print(" Rev: ");
+    Serial.print(REV);
+    Serial.print(" SWR: ");
+    Serial.print(VSWR);
+    Serial.print(" Ohms: ");
+    Serial.print(EffOhms);
+    Serial.println("");
+
+    // Use these lines instead of above if you want CSV output of frequency, FWD, REV, SWR, ohms.
+    // e.g. 7.160,24.21,5.92,1.65,82.39
+    //Serial.print(current_freq / 1000000);
+    //Serial.print(",");
+    //Serial.print(FWD);
+    //Serial.print(",");
+    //Serial.print(REV);
+    //Serial.print(",");
+    //Serial.print(VSWR);
+    //Serial.print(",");
+    //Serial.println(EffOhms);
+
     // Display on 2x16 serial LCD
-    //   7050 KHz 5.65
-    // XXXXXXXXXXXXXXXX
     // Freq 7050 kHz
-    // SWR 5.65 R 2000
+    // SWR 1.6 82.3Ω
 
     mySerial.print("\nFreq ");
     mySerial.print(int(current_freq / 1000));
     mySerial.print(" kHz\nSWR ");
     mySerial.print(VSWR, 1);
-    mySerial.print(" R ");
+    mySerial.print(" ");
     mySerial.print(EffOhms,1);
-    //delay(100);
+    mySerial.print("\xF4");
 
     current_freq += Step_Freq;
-    return RunCurve;
-}
-
-double CorrectReading(float ReadVal)
-{
-    if (ReadVal > 70)
-        return 0.8 * ReadVal + 57;
-
-    if (ReadVal < 13)
-        return 3.6 * (ReadVal * ReadVal) / 15;
-
-    float CalcVal = 1.1 * (8 + (2.1 * ReadVal) - ((ReadVal * ReadVal) * 10.7 / 1000));
-    return CalcVal;
+    return false;
 }
